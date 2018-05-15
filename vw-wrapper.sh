d33cd32789747f201651627a2b5458be6e4cd706
Cédric de Saint Martin <cdesaintmartin@wiremind.fr>
2018-05-15 17:11:20 +0200

    Fix cache-file.

--- vw-wrapper.sh
+++ vw-wrapper.sh
@@ -3,7 +3,7 @@
 VW="vw"
 SAVE_FILE="/app/saved-data/save.model"
 CACHE_FILE="/app/saved-data/vw.cache"
-PARAMS="--daemon --foreground -f $SAVE_FILE --cache_file $CACHE_FILE"
+PARAMS="--daemon --foreground -f $SAVE_FILE -c --cache_file $CACHE_FILE"
 
 if [ -f $SAVE_FILE ]; then
   echo "Previous model found: loading it."
